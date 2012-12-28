582b0638f33a28d67de003ff547fb53bdbdc8b8d
Ateev <ateev@live.com>
2012-12-28 20:23:47 +0530

    Update js/app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,27 @@
-var hashtag;
-var twitterapi = "http://search.twitter.com/search.json?q=Pink&rpp=1000" ;
-
-$.getJSON(twitterapi + 'q=' + hashtag + '&' + 'rpp=1500',  function(data){
+$(document).ready(setInterval(function () {
+  $('#search').submit(function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+    //var hashtag = $(this).find('input[name="hashtag"]').val();
+    var hashtag = $('input[name = hashtag]').val();
+    var twitterapi = "http://search.twitter.com/search.json?";
+    jQuery.ajax({
+      type: "GET",
+      url: twitterapi,
+      data: {
+        "q": hashtag,
+        "rpp": 1000
+      },
+      dataType: 'jsonp'
+    }).done(function (response) {
+      var results = response.results;
+      for (var i = 0; i < results.length; i++) {
+        $("#tweet").prepend("<p class='tweet'>" +
+          "<span class='username'>" +
+          results[i].from_user +
+          "</span> <span class='tweet_content'> " +
+          results[i].text + "</span></p>");
+      }
+    });
+  });
+}),7000);
